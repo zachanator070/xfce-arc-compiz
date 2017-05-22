3d128ba349f19aee1dbae39324ef42471048efac
Zach Johnson <zach.johnson070@gmail.com>
2017-05-22 08:10:58 -0600

    fixed revert greeter

--- uninstall.sh
+++ uninstall.sh
@@ -8,9 +8,8 @@ dconf write /org/gnome/metacity/theme "'Ambiance'"
 sudo apt remove compiz compizconfig-settings-manager
 
 # revert theme and icons
-sed -i '6s/.*/    <property name="IconThemeName" type="string" value="elementary-xfce"\/>' ~/.config/xfce4/xconf/xfce-perchannel-xml/xsettings.xml
-sed -i '5s/.*/    <property name="ThemeName" type="string" value="Ambiance"\/>' ~/.config/xfce4/xconf/xfce-perchannel-xml/xsettings.xml
-sed -i '40s/.*/    <property name="theme" type="string" value="Ambiance"\/>/' ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
+xfconf-query -c xsettings -p /Net/ThemeName -s "Greybird"
+xfconf-query -c xsettings -p /Net/IconThemeName -s "elementary-xfce-darkest"
 
 # remove icons
 cd arc-icon-theme
@@ -19,7 +18,7 @@ sudo rm -rf /usr/share/icons/Arc
 
 # revert greeter theme and icons
 > /ect/lightdm/lightdm-gtk-greeter.conf
-sudo echo "[greeter]\ntheme-name = Ambiance\nicon-theme-name = elementary-xfce" > /ect/lightdm/lightdm-gtk-greeter.conf
+sudo sh -c 'echo "[greeter]\ntheme-name = Greybird\nicon-theme-name = elementary-xfce-darkest" > /etc/lightdm/lightdm-gtk-greeter.conf'
 
 # remove arc theme and moka icons
 sudo apt-get remove arc-theme
